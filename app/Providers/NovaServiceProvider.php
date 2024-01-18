06f37a05980476440a198b88d396258db107c3b1
Kyle B. Johnson <kyle.johnson@hey.com>
2024-01-17 19:30:19 -0500

    chore: Enable Nova for all registered users

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -92,9 +92,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                'kyle.johnson@hey.com',
-            ]);
+            return true;
         });
     }
 
